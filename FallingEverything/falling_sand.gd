373dc3f4e76f1f927dbd19b6a02e92b05051adc5
YongjianWang <wangyongjian_501@hotmail.com>
2026-06-23 20:54:22 +0800

    在落沙模拟中加入刚体

--- FallingEverything/falling_sand.gd
+++ FallingEverything/falling_sand.gd
@@ -7,26 +7,46 @@ enum PixelMaterial {None, Air, Sand}
 
 var material_color = {
     PixelMaterial.Air: Color.WHITE,
-    PixelMaterial.Sand: Color.BLACK
+    PixelMaterial.Sand: Color.BLUE
 }
 var materials: PackedByteArray
 var image: Image
 var check_order := 0
 
+@onready var rigidbody = $RigidBody2D
+@onready var rigidbody_sprite = $RigidBody2D/Sprite2D
+@onready var rigidbody_texture = rigidbody_sprite.texture
+@onready var rigidbody_image: Image = rigidbody_texture.get_image()
+
+var collision_polygons: Array[CollisionPolygon2D] = []
+
 func _ready() -> void:
     materials.resize(width * height)
     materials.fill(PixelMaterial.Air)
 
-    # for i in range(materials.size()):
-    #     if randi_range(0, 10) == 0:
-    #         materials[i] = PixelMaterial.Sand
+    for i in range(width):
+        materials[i + (width - 1) * height] = PixelMaterial.Sand
 
     image = Image.create(width, height, false, Image.FORMAT_RGBA8)
     texture = ImageTexture.create_from_image(image)
 
     texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
 
+    offset = Vector2(width / 2.0, height / 2.0)
+
 func _process(_delta: float) -> void:
+    var rigidbody_pixels = {}
+    for i in range(ceil(rigidbody_image.get_width() * rigidbody_sprite.scale.x)):
+        for j in range(ceil(rigidbody_image.get_height() * rigidbody_sprite.scale.y)):
+            @warning_ignore("shadowed_variable_base_class")
+            var offset = Vector2(-rigidbody_image.get_width() * rigidbody_sprite.scale.x / 2.0, -rigidbody_image.get_height() * rigidbody_sprite.scale.y / 2.0)
+            var pos = rigidbody.transform * (Vector2(i, j) + offset)
+            var x = int(pos.x)
+            var y = int(pos.y)
+            if !(0 <= x && x < width) or !(0 <= y && y < height):
+                continue
+            rigidbody_pixels[Vector2i(x, y)] = rigidbody_image.get_pixelv(Vector2i(Vector2(i, j) / rigidbody_sprite.scale))
+
     for y in range(height - 1, -1, -1):
         for x in range(width):
             var index = y * width + x
@@ -48,46 +68,69 @@ func _process(_delta: float) -> void:
 
             match materials[index]:
                 PixelMaterial.Sand:
-                    if down == PixelMaterial.Air:
+                    if down == PixelMaterial.Air and Vector2i(x, y + 1) not in rigidbody_pixels:
                         materials[index] = PixelMaterial.Air
                         materials[down_index] = PixelMaterial.Sand
                     elif check_order == 0:
-                        if left_down == PixelMaterial.Air:
+                        if left_down == PixelMaterial.Air and Vector2i(x - 1, y + 1) not in rigidbody_pixels:
                             materials[index] = PixelMaterial.Air
                             materials[left_down_index] = PixelMaterial.Sand
-                        elif right_down == PixelMaterial.Air:
+                        elif right_down == PixelMaterial.Air and Vector2i(x + 1, y + 1) not in rigidbody_pixels:
                             materials[index] = PixelMaterial.Air
                             materials[right_down_index] = PixelMaterial.Sand
                     elif check_order == 1:
-                        if right_down == PixelMaterial.Air:
+                        if right_down == PixelMaterial.Air and Vector2i(x + 1, y + 1) not in rigidbody_pixels:
                             materials[index] = PixelMaterial.Air
                             materials[right_down_index] = PixelMaterial.Sand
-                        elif left_down == PixelMaterial.Air:
+                        elif left_down == PixelMaterial.Air and Vector2i(x - 1, y + 1) not in rigidbody_pixels:
                             materials[index] = PixelMaterial.Air
                             materials[left_down_index] = PixelMaterial.Sand
             check_order = 1 - check_order
 
+    var bitmap := BitMap.new()
+    bitmap.create(image.get_size())
+
     var pixels := image.get_data()
-    for i in range(materials.size()):
-        var c: Color = material_color[materials[i]]
-        var index := i * 4
-        pixels[index] = int(c.r * 255)
-        pixels[index + 1] = int(c.g * 255)
-        pixels[index + 2] = int(c.b * 255)
-        pixels[index + 3] = 255
+    for x in range(width):
+        for y in range(height):
+            var index = y * width + x
+            if materials[index] == PixelMaterial.Sand:
+                bitmap.set_bit(x, y, true)
+
+            var c: Color = material_color[materials[index]]
+            index = index * 4
+            pixels[index] = int(c.r * 255)
+            pixels[index + 1] = int(c.g * 255)
+            pixels[index + 2] = int(c.b * 255)
+            pixels[index + 3] = 255
     image.set_data(width, height, false, Image.FORMAT_RGBA8, pixels)
+
+    for key in rigidbody_pixels:
+        image.set_pixelv(key, rigidbody_pixels[key])
+
     texture.update(image)
+
+    var polygons = bitmap.opaque_to_polygons(Rect2(Vector2(), image.get_size()), 0)
+    var i = 0
+    while i < polygons.size():
+        var polygon = polygons[i]
+        if i >= collision_polygons.size():
+            var static_body = StaticBody2D.new()
+            var collision_polygon = CollisionPolygon2D.new()
+            static_body.add_child(collision_polygon)
+            add_child(static_body)
+            collision_polygons.push_back(collision_polygon)
+        collision_polygons[i].polygon = polygon
+        i += 1
+    while i < collision_polygons.size():
+        var collision_polygon = collision_polygons.pop_back()
+        remove_child(collision_polygon.get_parent())
 
     if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
         var mouse_pos: Vector2 = get_local_mouse_position()
-        var x := int(mouse_pos.x + width / 2.0)
-        var y := int(mouse_pos.y + height / 2.0)
+        var x := int(mouse_pos.x)
+        var y := int(mouse_pos.y)
 
-        for i in range(0, 1):
-            for j in range(0, 1):
-                var index = (y + j) * width + x + i
-                # if randi_range(0, 10) != 0:
-                #     continue
-                if index >= materials.size():
-                    continue
-                materials[index] = PixelMaterial.Sand
+        var index = y * width + x
+        if 0 <= index and index < materials.size():
+            materials[index] = PixelMaterial.Sand
